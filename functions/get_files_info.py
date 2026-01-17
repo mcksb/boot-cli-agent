55dd5d3eeb62785b3c65ada0a8f80949af84fa43
mcksb <shaunmcmahon67@gmail.com>
2026-01-17 00:07:59 +0000

    Update

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,9 +1,18 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    wd_path = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(wd_path, directory))
+    
+    is_valid_directory = os.path.isdir(directory)
+    if is_valid_directory == False:
+        raise Exception(f'Error: "{directory}" is not a directory')
+
+    absolute_path = os.path.abspath(working_directory)
+    target_directory = os.path.normpath(os.path.join(absolute_path, directory))
+    
     # Will be True or False
-    valid_target_dir = os.path.commonpath([wd_path, target_dir]) == wd_path
+    valid_target_dir = os.path.commonpath([absolute_path, target_directory]) == absolute_path
+    
     if valid_target_dir == False:
-        raise Exception(f'Error: Cannot list {directory} as it is outside the permitted working directory') 
+        raise Exception(f'Error: Cannot list {directory} as it is outside the permitted working directory')
+
+    return
