366a1e5de571eded6db54e652f1d3782f8330138
mcksb <shaunmcmahon67@gmail.com>
2026-01-20 23:49:25 +0000

    Update: functions/get_files_info.py; Added: test_get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,18 +1,28 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    
-    is_valid_directory = os.path.isdir(directory)
-    if is_valid_directory == False:
-        raise Exception(f'Error: "{directory}" is not a directory')
+    try:
+        absolute_path = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(absolute_path, directory))
+        
+        is_directory = os.path.isdir(target_path)
+        if is_directory != True:
+            return f'Error: "{directory}" is not a directory'
 
-    absolute_path = os.path.abspath(working_directory)
-    target_directory = os.path.normpath(os.path.join(absolute_path, directory))
-    
-    # Will be True or False
-    valid_target_dir = os.path.commonpath([absolute_path, target_directory]) == absolute_path
-    
-    if valid_target_dir == False:
-        raise Exception(f'Error: Cannot list {directory} as it is outside the permitted working directory')
+        common_path = os.path.commonpath([absolute_path, target_path]) == absolute_path
+        if common_path != True:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    return
+        directory_contents = os.listdir(target_path)
+        file_info = ''
+        for file in directory_contents:
+            file_path = os.path.join(target_path, file)
+            file_name = file
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
+            file_info = file_info + f'- {file}: file_size={file_size}, is_dir={is_dir}\n'
+    
+    except Exception as e:
+        return f'Error: {e}'
+    
+    return file_info
